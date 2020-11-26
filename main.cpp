db4781fc58fa2dd8a71aaa27d94f7dc0854c23a8
snuczek <26097711+snuk0@users.noreply.github.com>
2020-11-26 13:10:27 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -5,10 +5,21 @@ using namespace std;
 
 vector <int> liczby;
 
-void r(int x){
+int p() {
+    int ile = 0;
+    for (int i = 0; i < liczby.size(); i++) {
+        if (i == 0 || liczby[i] != liczby[i - 1]) {
+            if (liczby[i] % 2 == 1)
+                ile++;
+        }
+    }
+    return ile;
+}
+
+void r(int x) {
     liczby.clear();
-    for (int i = 2; i * i <= x; i++){
-        while (x % i == 0){
+    for (int i = 2; i * i <= x; i++) {
+        while (x % i == 0) {
             liczby.push_back(i);
             x /= i;
         }
@@ -17,22 +28,12 @@ void r(int x){
         liczby.push_back(x);
 }
 
-int p(){
-    int ile = 0;
-    for (int i = 0; i < liczby.size(); i++){
-        if (i == 0 || liczby[i] != liczby[i - 1]){
-            if (liczby[i] % 2 == 1)
-                ile++;
-        }
-    }
-    return ile;
-}
 
-int main(){
+int main() {
     ifstream f1("liczby.txt");
-    if (f1.good()){
+    if (f1.good()) {
         int x, licznik = 0;
-        while (f1 >> x){
+        while (f1 >> x) {
             r(x);
             if (p() == 3)
                 licznik++;
